ce6bd90bdff9a46e850313090f9a8d6398d23cb7
Employee-94 <Employee-94@DESKTOP-PS1HIHS>
2025-12-31 18:19:37 +0530

    jpa Inital commit

---
+++ SpringBootPracticeWithJPA/src/main/java/com/in/kousiK/SpringBootPracticeWithJPA/SpringBootPracticeWithJpaApplication.java
@@ -0,0 +1,13 @@
+package com.in.kousiK.SpringBootPracticeWithJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPracticeWithJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPracticeWithJpaApplication.class, args);
+	}
+
+}
---
+++ SpringBootPracticeWithJPA/src/test/java/com/in/kousiK/SpringBootPracticeWithJPA/SpringBootPracticeWithJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in.kousiK.SpringBootPracticeWithJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPracticeWithJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
